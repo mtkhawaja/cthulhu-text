4e4d70f9c4090699a7800d85f22bd2bdf4baddd5
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2021-11-03 01:17:14 -0400

    Created spring project using Spring Initializr (https://start.spring.io/).

---
+++ src/main/java/com/cthulhutext/api/CthulhuTextApiApplication.java
@@ -0,0 +1,13 @@
+package com.cthulhutext.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CthulhuTextApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CthulhuTextApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cthulhutext/api/CthulhuTextApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cthulhutext.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CthulhuTextApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
