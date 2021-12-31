73648e36a74821fefe6edf78b5cafe00c9936baa
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2021-12-31 18:58:48 -0500

    feat: Added property file for String constants.
    - Added src/main/resources/static/string.properties
    - Defined @PropertySource for string.properties in CthulhuTextApiApplication.java.

--- src/main/java/com/cthulhutext/api/CthulhuTextApiApplication.java
+++ src/main/java/com/cthulhutext/api/CthulhuTextApiApplication.java
@@ -2,10 +2,12 @@ package com.cthulhutext.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@PropertySource("classpath:/static/string.properties")
 public class CthulhuTextApiApplication {
 
     public static void main(String[] args) {
