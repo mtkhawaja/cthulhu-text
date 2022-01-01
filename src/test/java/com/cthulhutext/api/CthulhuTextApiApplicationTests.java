a47e2033a355515b7721a551a0c0d9065e067c34
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2021-12-31 20:38:08 -0500

    fix: Added assertion for default application context load test. (src/test/java/com/cthulhutext/api/CthulhuTextApiApplicationTests.java)

--- src/test/java/com/cthulhutext/api/CthulhuTextApiApplicationTests.java
+++ src/test/java/com/cthulhutext/api/CthulhuTextApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cthulhutext.api;
 
+import com.cthulhutext.api.welcome.WelcomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CthulhuTextApiApplicationTests {
+    @Autowired
+    WelcomeController welcomeController;
 
     @Test
     void contextLoads() {
+        assertThat(welcomeController).isNotNull();
     }
 
 }
