5f9f78015b3e1896fd3129969b05faa9c4adf5c1
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2022-01-02 03:47:19 -0500

    feat: Added happy path integration test for generating cthulhu-fied text using intensity parameters.

--- src/test/java/com/cthulhutext/api/conversion/TextConversionControllerHttpTest.java
+++ src/test/java/com/cthulhutext/api/conversion/TextConversionControllerHttpTest.java
@@ -13,30 +13,49 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TextConversionControllerHttpTest {
+    private final String text = "God! What wonder that across the earth a great architect went mad";
     @LocalServerPort
     private int port;
-
     @Autowired
     private TestRestTemplate testRestTemplate;
 
     private String baseEndpoint;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         baseEndpoint = String.format("http://localhost:%s/v1/convert/", port);
     }
 
     @DisplayName("Should generate CursedText When the conversion endpoint is hit")
     @Test
     void shouldGenerateCursedTextWhenTheConversionEndpointIsHit() {
-        String text = "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.";
         String endpoint = baseEndpoint + text;
         CursedText cursedText = testRestTemplate.getForObject(endpoint, CursedText.class);
         assertThat(cursedText).isNotNull();
         assertThat(cursedText.content())
                 .isNotNull()
                 .hasSizeGreaterThanOrEqualTo(text.length());
-
     }
+
+    @DisplayName("Should generate CursedText from text and intensity parameters When the conversion endpoint is hit")
+    @Test
+    void shouldGenerateCursedTextFromTextAndIntensityParametersWhenTheConversionEndpointIsHit() {
+        int upperIntensity = 1;
+        int middleIntensity = 5;
+        int lowerIntensity = 6;
+        int outputLength = (upperIntensity + middleIntensity + lowerIntensity) * text.length();
+        String endpoint = createEndpointWithIntensities(upperIntensity, middleIntensity, lowerIntensity);
+        CursedText cursedText = testRestTemplate.getForObject(endpoint, CursedText.class);
+        assertThat(cursedText).isNotNull();
+        assertThat(cursedText.content())
+                .isNotNull()
+                .hasSize(text.length() + outputLength);
+    }
+
+    private String createEndpointWithIntensities(int upperIntensity, int middleIntensity, int lowerIntensity) {
+        return String.format("%s%s?upperIntensity=%s&lowerIntensity=%s&middleIntensity=%s",
+                baseEndpoint, text, upperIntensity, middleIntensity, lowerIntensity);
+    }
+
 
 }
