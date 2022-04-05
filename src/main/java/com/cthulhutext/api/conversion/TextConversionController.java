e5c5bfc6133610727426f829b2e9b94af5974265
Muneeb Khawaja <muneebtkhawaja@gmail.com>
2022-04-04 23:34:32 -0400

    feat: Migrated from `springfox-swagger-2` to `springdoc-openapi-ui` (1.6.6)

--- src/main/java/com/cthulhutext/api/conversion/TextConversionController.java
+++ src/main/java/com/cthulhutext/api/conversion/TextConversionController.java
@@ -1,9 +1,9 @@
 package com.cthulhutext.api.conversion;
 
 import com.cthulhutext.api.models.CursedText;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,27 +15,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-@Api(tags = "Text Conversion", value = "Set of endpoints for converting text to Cthulhu-fied text.")
+@Tag(name = "Text Conversion", description = "Set of endpoints for converting text to Cthulhu-fied text.")
 @RequestMapping("/v1/convert")
 @Validated
 public interface TextConversionController {
-    @ApiOperation(value = "getCthulhuText", notes = "Generated Cthulhu-fied text using random intensities.")
+    @Operation(summary = "getCthulhuText", description = "Generated Cthulhu-fied text using random intensities.")
     @GetMapping(value = "/random/{text}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<CursedText> getCthulhuText(
-            @ApiParam(value = "The text to be converted", required = true, example = "Damn it, it wasn't quite fresh enough!")
+            @Parameter(description = "The text to be converted", required = true, example = "Damn it, it wasn't quite fresh enough!")
             @PathVariable String text
     );
 
-    @ApiOperation(value = "getCthulhuText", notes = "Generate Cthulhu-fied text.")
+    @Operation(summary = "getCthulhuText", description = "Generate Cthulhu-fied text.")
     @GetMapping(value = "/{text}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<CursedText> getCthulhuText(
-            @ApiParam(value = "The text to be converted", required = true, example = "This is a dream. It's fiction.")
+            @Parameter(description = "The text to be converted", required = true, example = "This is a dream. It's fiction.")
             @PathVariable String text,
-            @ApiParam(value = "The intensity [1 - 100] of upper diacritical marks such as U+0300, U+0301 etc.", allowableValues = "range[1,100]")
+            @Parameter(description = "The intensity [1 - 100] of upper diacritical marks such as U+0300, U+0301 etc.")
             @RequestParam(defaultValue = "1", value = "upperIntensity") @Min(1) @Max(100) int upperIntensity,
-            @ApiParam(value = "The intensity [1 - 100] of middle diacritical marks such as U+0310, U+0311 etc.", allowableValues = "range[1,100]")
+            @Parameter(description = "The intensity [1 - 100] of middle diacritical marks such as U+0310, U+0311 etc.")
             @RequestParam(defaultValue = "1", value = "middleIntensity") @Min(1) @Max(100) int middleIntensity,
-            @ApiParam(value = "The intensity [1 - 100] of lower diacritical marks such as U+0316, U+0317 etc.", allowableValues = "range[1,100]")
+            @Parameter(description = "The intensity [1 - 100] of lower diacritical marks such as U+0316, U+0317 etc.")
             @RequestParam(defaultValue = "1", value = "lowerIntensity") @Min(1) @Max(100) int lowerIntensity
     );
 }
--- src/main/java/com/cthulhutext/api/welcome/WelcomeController.java
+++ src/main/java/com/cthulhutext/api/welcome/WelcomeController.java
@@ -1,7 +1,7 @@
 package com.cthulhutext.api.welcome;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
-@Api(tags = "Welcome Controller")
+@Tag(name = "Welcome Controller")
 @RequestMapping({"v1"})
 public interface WelcomeController {
-    @ApiOperation(value = "Displays Welcome message.")
+    @Operation(description = "Displays Welcome message.")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Map<String, String>> sayHello();
-
-
 }
